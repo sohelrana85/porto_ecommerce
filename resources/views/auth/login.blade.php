2fdb95a55e2464fe808f4f32aa32a7eca5f66264
sohelrana85 <75238575+sohelrana85@users.noreply.github.com>
2021-01-12 21:08:56 +0600

    first class done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -13,44 +13,145 @@
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
-            @csrf
+@csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+<!-- Email Address -->
+<div>
+    <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+</div>
+
+<!-- Password -->
+<div class="mt-4">
+    <x-label for="password" :value="__('Password')" />
+
+    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+        autocomplete="current-password" />
+</div>
+
+<!-- Remember Me -->
+<div class="block mt-4">
+    <label for="remember_me" class="inline-flex items-center">
+        <input id="remember_me" type="checkbox"
+            class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            name="remember">
+        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+    </label>
+</div>
+
+<div class="flex items-center justify-end mt-4">
+    @if (Route::has('password.request'))
+    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+        {{ __('Forgot your password?') }}
+    </a>
+    @endif
+
+    <x-button class="ml-3">
+        {{ __('Login') }}
+    </x-button>
+</div>
+</form>
+</x-auth-card>
+</x-guest-layout> --}}
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from codervent.com/syndash/demo/vertical/authentication-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 09 Jan 2021 15:52:31 GMT -->
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Porto Ecommerce</title>
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png') }}" type="image/png" />
+    <!-- loader-->
+    <link href="{{ asset('backend/assets/css/pace.min.css" rel="stylesheet') }}" />
+    <script src="{{ asset('backend/assets/js/pace.min.js') }}"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/bootstrap.min.css') }}" />
+    <!-- Icons CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/icons.css') }}" />
+    <!-- App CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/app.css') }}" />
+</head>
+
+<body class="bg-login">
+    <!-- wrapper -->
+    <div class="wrapper">
+        <div class="section-authentication-login d-flex align-items-center justify-content-center">
+            <div class="row">
+                <div class="col-12 col-lg-10 mx-auto">
+                    <div class="card radius-15">
+                        <div class="row no-gutters">
+                            <div class="col-lg-6">
+                                <div class="card-body p-md-5">
+                                    <div class="text-center">
+                                        <img src="{{ asset('backend/assets/images/logo-icon.png') }}" width="80" alt="">
+                                        <h3 class="mt-4 font-weight-bold">Welcome Back</h3>
+                                    </div>
+
+                                    <div class="login-separater text-center"> <span>OR LOGIN WITH EMAIL</span>
+                                        <hr />
+                                    </div>
+                                    <div class="form-group mt-4">
+                                        <label>Email Address</label>
+                                        <input type="text" class="form-control"
+                                            placeholder="Enter your email address" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control" placeholder="Enter your password" />
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="form-group col">
+                                            <div class="custom-control custom-switch">
+                                                <input type="checkbox" class="custom-control-input" id="customSwitch1"
+                                                    checked>
+                                                <label class="custom-control-label" for="customSwitch1">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <div class="form-group col text-right"> <a
+                                                href="authentication-forgot-password.html"><i
+                                                    class='bx bxs-key mr-2'></i>Forget Password?</a>
+                                        </div>
+                                    </div>
+                                    <div class="btn-group mt-3 w-100">
+                                        <button type="button" class="btn btn-primary btn-block">Log In</button>
+                                        <button type="button" class="btn btn-primary"><i
+                                                class="lni lni-arrow-right"></i>
+                                        </button>
+                                    </div>
+                                    <hr>
+                                    <div class="text-center">
+                                        <p class="mb-0">Don't have an account? <a
+                                                href="authentication-register.html">Sign
+                                                up</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="{{ asset('backend/assets/images/login-images/login-frent-img.jpg') }}"
+                                    class="card-img login-img h-100" alt="...">
+                            </div>
+                        </div>
+                        <!--end row-->
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+    <!-- end wrapper -->
+</body>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+<!-- Mirrored from codervent.com/syndash/demo/vertical/authentication-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 09 Jan 2021 15:52:31 GMT -->
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Login') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
